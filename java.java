a6785417222fde9f4199c86d8af0477f31bf6c40
Jothis1234 <hicaslab2006@gmail.com>
2025-07-04 15:13:13 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,46 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        String place = "Coimbatore";
+        String title = "Software Engineer";
+        System.out.print(place+" "+title);
+    }
+}
+class Main {
+public static void main(String[] args)
+{
+    String FirstName = "Jothis";
+    String Lastname = "Senthilkumar";
+    int age = 18;
+    System.out.println("FirstName:" + FirstName +" "+ Lastname);
+    System.out.println("My age is" + " "+ age); 
+    
+    
+}  
+}
+class Main {
+    public static void main(String[] args)
+    {
+    int birth_year = 2006;
+    int Future_year = 2025;
+    int age = Future_year - birth_year;
+     System.out.println(age);
+    }
+    
+}
+class Main {
+    public static void main(String[] args)
+    {
+        int current_age = 20;
+        int maximum_age = 90;
+        int snacks = 100;
+        int remaining_age = maximum_age - current_age;
+        int a = remaining_age*365;
+        int total_spend = a*snacks;
+        System.out.println(remaining_age);
+        System.out.println(total_spend);
+    }
+}
+        
